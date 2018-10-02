c9494a442c65409d0105b96b1d6e6a728f1a48b0
user <emilberzin@gmail.com>
2018-10-02 14:00:14 +0300

    20181002_13_59

---
+++ dgr_20181002.py
@@ -0,0 +1,154 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> constants
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    constants
+NameError: name 'constants' is not defined
+>>> print( sveiki)
+
+Traceback (most recent call last):
+  File "<pyshell#1>", line 1, in <module>
+    print( sveiki)
+NameError: name 'sveiki' is not defined
+>>> print (220)
+220
+>>> x=20
+>>> y=20
+>>> x= x+20
+>>> print(x)
+40
+>>> xx=10
+>>> xx=xx+12
+>>> print(xx)
+22
+>>> xx=y%2
+>>> print(xx)
+0
+>>> z=2202002022+2378*20
+>>> print(z)
+2202049582
+>>> inp=input("vai tu lasi ?")
+vai tu lasi ?
+
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    inp=input("vai tu lasi ?")
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> usf=int(inp) ja
+SyntaxError: invalid syntax
+>>> inp=input("europe floor?")
+europe floor?
+
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    inp=input("europe floor?")
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> usf=int(inp)+ 1
+
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    usf=int(inp)+ 1
+NameError: name 'inp' is not defined
+>>> usf=int(inp) + 1
+
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    usf=int(inp) + 1
+NameError: name 'inp' is not defined
+>>> nam=input("Kas tu esi")
+Kas tu esi
+
+Traceback (most recent call last):
+  File "<pyshell#19>", line 1, in <module>
+    nam=input("Kas tu esi")
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> print("Welcome",nam)
+
+Traceback (most recent call last):
+  File "<pyshell#20>", line 1, in <module>
+    print("Welcome",nam)
+NameError: name 'nam' is not defined
+>>> inp=input("sveiks") #pievienoju komentaru
+sveiks
+
+Traceback (most recent call last):
+  File "<pyshell#21>", line 1, in <module>
+    inp=input("sveiks") #pievienoju komentaru
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> print(float(99) +100)
+199.0
+>>> i=42
+>>> type(i)
+<type 'int'>
+>>> f=float(i)
+>>> print(f)
+42.0
+>>> type(f)
+<type 'float'>
+>>> print(10/2)
+5
+>>> print( 200000000000212112/3333)
+60006000600123
+>>> sval='123'
+>>> type(sval)
+<type 'str'>
+>>> print(sval + 1)
+
+Traceback (most recent call last):
+  File "<pyshell#32>", line 1, in <module>
+    print(sval + 1)
+TypeError: cannot concatenate 'str' and 'int' objects
+>>> ival=int(sval)
+>>> type(ival)
+<type 'int'>
+>>> print(ival +1)
+124
+>>> dd=1+5
+>>> print(dd)
+6
+>>> ee="Labs" + "rits"
+>>> print(ee)
+Labsrits
+>>> a=200
+>>> b=111111111111111
+>>> c=a*b
+>>> print(c)
+22222222222222200
+>>> inp=input('Europes floor heigth?')
+Europes floor heigth?
+
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    inp=input('Europes floor heigth?')
+  File "<string>", line 0
+    
+    ^
+SyntaxError: unexpected EOF while parsing
+>>> usf=int(inp) + 1
+
+Traceback (most recent call last):
+  File "<pyshell#45>", line 1, in <module>
+    usf=int(inp) + 1
+NameError: name 'inp' is not defined
+>>> print('us floor',usf)
+
+Traceback (most recent call last):
+  File "<pyshell#46>", line 1, in <module>
+    print('us floor',usf)
+NameError: name 'usf' is not defined
+>>> 
